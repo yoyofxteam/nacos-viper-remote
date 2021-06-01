badd6647dcccc09bc2f56b0b298bc870d514ed10
yoyofx <zlhxd@hotmail.com>
2021-06-01 19:33:47 +0800

    Nacos AMC or KMS

--- nacos_manager.go
+++ nacos_manager.go
@@ -34,9 +34,15 @@ func NewNacosConfigManager(option *Option) (*nacosConfigManager, error) {
 		MaxAge:              3,
 		LogLevel:            "info",
 	}
+
 	if option.Auth != nil && option.Auth.Enable {
 		clientConfig.Username = option.Auth.User
 		clientConfig.Password = option.Auth.Password
+		clientConfig.Endpoint = option.Auth.Endpoint
+		clientConfig.RegionId = option.Auth.RegionId
+		clientConfig.AccessKey = option.Auth.AccessKey
+		clientConfig.SecretKey = option.Auth.SecretKey
+		clientConfig.OpenKMS = option.Auth.OpenKMS
 	}
 	client, err := clients.CreateConfigClient(map[string]interface{}{
 		"serverConfigs": serverConfigs,
